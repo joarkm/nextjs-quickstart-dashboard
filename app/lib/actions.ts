bc4b45001665d87e33e16ccee93d686dca2464ad
Joar Krohn Mauseth <jkm99412@gmail.com>
2024-06-08 12:52:44 +0200

    Chapter 14 - Add form validation to edit invoice

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -65,12 +65,24 @@ export async function createInvoice(_prevState: State, formData: FormData) {
 
 const UpdateInvoice = FormSchema.omit({ id: true, date: true });
 
-export async function updateInvoice(id: string, formData: FormData) {
-  const { customerId, amount, status } = UpdateInvoice.parse({
+export async function updateInvoice(
+  id: string,
+  _prevState: State,
+  formData: FormData,
+) {
+  const validatedFields = UpdateInvoice.safeParse({
     customerId: formData.get('customerId'),
     amount: formData.get('amount'),
     status: formData.get('status'),
   });
+  // If form validation fails, return errors early. Otherwise, continue.
+  if (!validatedFields.success) {
+    return {
+      errors: validatedFields.error.flatten().fieldErrors,
+      message: 'Missing Fields. Failed to Update Invoice.',
+    };
+  }
+  const { customerId, amount, status } = validatedFields.data;
 
   const amountInCents = amount * 100;
 
